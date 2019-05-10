5b284d142c4af6f2c80255a846762119a697ea18
Jameson Graef Rollins <jrollins@finestructure.net>
2019-05-10 16:17:57 -0700

    code cleanup in main (no functional change)

--- gpstime/__main__.py
+++ gpstime/__main__.py
@@ -5,39 +5,48 @@ from dateutil.tz import tzutc, tzlocal
 import ietf_leap_seconds
 from . import ISO_FORMAT, gpstime, GPSTimeException
 
+
+PARSER = argparse.ArgumentParser(
+    description="""GPS time conversion
+
+Print local, UTC, and GPS time for the specified time string.
+""",
+    epilog="""See the python datetime module for time formating options:
+https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+""",
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+PARSER.add_argument(
+    '-v', '--version', action='store_true',
+    help="print version number and exit")
+zg = PARSER.add_mutually_exclusive_group()
+zg.add_argument(
+    '-l', '--local', action='store_const', dest='tz', const='local',
+    help="print only local time")
+zg.add_argument(
+    '-u', '--utc', action='store_const', dest='tz', const='utc',
+    help="print only UTC time")
+zg.add_argument(
+    '-g', '--gps', action='store_const', dest='tz', const='gps',
+    help="print only GPS time")
+fg = PARSER.add_mutually_exclusive_group()
+fg.set_defaults(format='%Y-%m-%d %H:%M:%S.%f %Z')
+fg.add_argument(
+    '-i', '--iso', action='store_const', dest='format', const=ISO_FORMAT,
+    help="use ISO time format")
+fg.add_argument(
+    '-f', '--format',
+    help="specify time format (see below)")
+PARSER.add_argument(
+    'time', metavar='TIME', nargs=argparse.REMAINDER, default='now',
+    help="time string in any format (including GPS), or current time if not specified")
+
+
 def tzname(tz):
     return gpstime.now(tz).tzname()
 
+
 def main():
-    description = """GPS time conversion
-
-Print local, UTC, and GPS time for the specified time string.
-"""
-    epilog = """See the python datetime module for time formating options:
-https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
-"""
-    parser = argparse.ArgumentParser(description=description,
-                                     epilog=epilog,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-v', '--version', action='store_true',
-                        help="print version number and exit")
-    zg = parser.add_mutually_exclusive_group()
-    zg.add_argument('-l', '--local', action='store_const', dest='tz', const='local',
-                    help="print only local time")
-    zg.add_argument('-u', '--utc', action='store_const', dest='tz', const='utc',
-                    help="print only UTC time (default)")
-    zg.add_argument('-g', '--gps', action='store_const', dest='tz', const='gps',
-                    help="print only GPS time")
-    fg = parser.add_mutually_exclusive_group()
-    fg.set_defaults(format='%Y-%m-%d %H:%M:%S.%f %Z')
-    fg.add_argument('-i', '--iso', action='store_const', dest='format', const=ISO_FORMAT,
-                    help="use ISO time format")
-    fg.add_argument('-f', '--format',
-                    help="specify time format (see below)")
-    parser.add_argument('time', metavar='TIME', nargs=argparse.REMAINDER, default='now',
-                        help="time string in any format (including GPS), or current time if not specified")
-    args = parser.parse_args()
-
+    args = PARSER.parse_args()
     if args.version:
         print(__version__)
         sys.exit()
