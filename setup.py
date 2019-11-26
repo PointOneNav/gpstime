7a86e6ae7bc2d5997f47f0c1503b2080b00bbbc5
Jameson Graef Rollins <jrollins@finestructure.net>
2019-11-26 09:59:23 -0800

    don't import gpstime in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+import os
 from setuptools import setup
 
-from gpstime import __version__
+with open(os.path.join('gpstime', '__version__.py')) as f:
+    exec(f.read())
 
 with open('README.md', 'rb') as f:
     longdesc = f.read().decode().strip()
@@ -21,12 +23,19 @@ setup(
          'GNU General Public License v3 or later (GPLv3+)'),
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python3',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     packages=['gpstime'],
     scripts=['bin/gpstime'],
 
+    tests_require=[
+        'python-dateutil',
+    ],
     install_requires=[
         'python-dateutil',
         'requests',
